16ab0b5af5829ff45aeb7202ac4b96a103041f04
Dhaniele Demontano <dhane1000@gmail.com>
2023-04-24 18:56:46 +0800

    added project files

---
+++ BlazorServerDevExpXAF.Blazor.Server/Startup.cs
@@ -0,0 +1,131 @@
+﻿using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor.Services;
+using DevExpress.Persistent.Base;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using DevExpress.ExpressApp.Xpo;
+using BlazorServerDevExpXAF.Blazor.Server.Services;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using System.Security.Principal;
+using System.Security.Claims;
+using Microsoft.Identity.Web;
+using DevExpress.ExpressApp.Core;
+using BlazorServerDevExpXAF.Helpers;
+
+namespace BlazorServerDevExpXAF.Blazor.Server;
+
+public class Startup {
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddSingleton(typeof(Microsoft.AspNetCore.SignalR.HubConnectionHandler<>), typeof(ProxyHubConnectionHandler<>));
+
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddHttpContextAccessor();
+        services.AddScoped<CircuitHandler, CircuitHandlerProxy>();
+        services.AddXaf(Configuration, builder => {
+            builder.UseApplication<BlazorServerDevExpXAFBlazorApplication>();
+            builder.Modules
+                .AddCloningXpo()
+                .AddConditionalAppearance()
+                .AddReports(options => {
+                    options.EnableInplaceReports = true;
+                    options.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+                    options.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+                    options.ShowAdditionalNavigation = true;
+                })
+                .AddValidation(options => {
+                    options.AllowValidationDetailsAccess = false;
+                })
+                .Add<BlazorServerDevExpXAF.Module.BlazorServerDevExpXAFModule>()
+            	.Add<BlazorServerDevExpXAFBlazorModule>();
+            builder.ObjectSpaceProviders
+                .AddSecuredXpo((serviceProvider, options) => {
+                    string connectionString = null;
+                    if(Configuration.GetConnectionString("ConnectionString") != null) {
+                        connectionString = Configuration.GetConnectionString("ConnectionString");
+                    }
+                    #if EASYTEST
+                    if(Configuration.GetConnectionString("EasyTestConnectionString") != null) {
+                        connectionString = Configuration.GetConnectionString("EasyTestConnectionString");
+                    }
+                    #endif
+                    ArgumentNullException.ThrowIfNull(connectionString);
+                    options.ConnectionString = connectionString;
+                    options.ThreadSafe = true;
+                    options.UseSharedDataStoreProvider = true;
+                })
+                .AddNonPersistent();
+            builder.Security
+                .UseIntegratedMode(options => {
+                    options.RoleType = typeof(PermissionPolicyRole);
+                    // ApplicationUser descends from PermissionPolicyUser and supports the OAuth authentication. For more information, refer to the following topic: https://docs.devexpress.com/eXpressAppFramework/402197
+                    // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+                    options.UserType = typeof(BlazorServerDevExpXAF.Module.BusinessObjects.ApplicationUser);
+                    // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+                    // If you use PermissionPolicyUser or a custom user type, comment out the following line:
+                    options.UserLoginInfoType = typeof(BlazorServerDevExpXAF.Module.BusinessObjects.ApplicationUserLoginInfo);
+                    options.UseXpoPermissionsCaching();
+                })
+                .AddPasswordAuthentication(options => {
+                    options.IsSupportChangePassword = true;
+                })
+                .AddAuthenticationProvider<CustomAuthenticationProvider>();
+        });
+        var authentication = services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
+        authentication
+            .AddCookie(options => {
+                options.LoginPath = "/LoginPage";
+            });
+        //Configure OAuth2 Identity Providers based on your requirements. For more information, see
+        //https://docs.devexpress.com/eXpressAppFramework/402197/task-based-help/security/how-to-use-active-directory-and-oauth2-authentication-providers-in-blazor-applications
+        //https://developers.google.com/identity/protocols/oauth2
+        //https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow
+        //https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow
+        authentication.AddMicrosoftIdentityWebApp(Configuration, configSectionName: "Authentication:AzureAd", cookieScheme: null);
+
+        string connectionString = Configuration.GetConnectionString("ConnectionString") ?? string.Empty;
+        IDataStore dataStore = XpoDefault.GetConnectionProvider(connectionString, AutoCreateOption.DatabaseAndSchema);
+        dataStore = new DataCacheRoot(dataStore);
+        WebApiDataStoreService service = new(dataStore);
+        services.AddSingleton(dataStore);
+        services.AddSingleton(service);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if(env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. To change this for production scenarios, see: https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseXpoDemoData();
+        app.UseHttpsRedirection();
+        app.UseRequestLocalization();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseXaf();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapXafEndpoints();
+            endpoints.MapBlazorHub();
+            endpoints.MapFallbackToPage("/_Host");
+            endpoints.MapControllers();
+        });
+    }
+}
---
+++ BlazorServerDevExpXAF.Module/BusinessObjects/XPO/ORMDataModelCode/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+
+namespace BlazorServerDevExpXAF.Module.BusinessObjects.XPO.DxSample
+{
+    [NavigationItem(true)]
+    public partial class Customer
+    {
+        public Customer(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
